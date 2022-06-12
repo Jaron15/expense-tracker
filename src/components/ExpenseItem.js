a170c5abe6e825b332327360582a3fd39d6a0aa9
Jaron <jjkenyon777@gmail.com>
2022-06-11 20:24:25 -0700

    refactor Expense Item components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
-    <div className='expense-item'>
-      <div>March 28th 2021</div>
+    <div className="expense-item">
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>Car insurance</h2>
-        <div className='expense-item__price'>$249.67</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
